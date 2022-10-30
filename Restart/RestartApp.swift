876cba0e0d8d09cd9c0514d7c2087335592266f8
Prithvi Raj <Prithvi2229@gmail.com>
2022-10-30 19:29:37 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Prithviraj Patil on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
